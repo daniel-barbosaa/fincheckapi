3ebab6996a9c7ab526979dac101afa7140cb5efa
daniel-barbosaa <danimendes9728@gmail.com>
2025-05-16 17:07:51 -0300

    feat: projeto inicial

---
+++ prisma/migrations/20250516181608_create_banck_accounts_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "BancAccountType" AS ENUM ('CHECKING', 'INVESTMENT', 'CASH');
+
+-- CreateTable
+CREATE TABLE "bank_accounts" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" "BancAccountType" NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "bank_accounts_pkey" PRIMARY KEY ("id")
+);

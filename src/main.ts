c0e967556a634bb9ee98407d92f2baa2aacaa486
daniel-barbosaa <danimendes9728@gmail.com>
2025-12-23 15:02:17 -0300

    fix: Solving build error

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors({
-        origin: process.env.CORS_ORIGIN,
+        origin: process.env.CORS_ORIGIN?.split(',') || '*',
+        credentials: true,
     });
     if (process.env.NODE_ENV !== 'production') {
         await app.listen(process.env.PORT ?? 3001);
